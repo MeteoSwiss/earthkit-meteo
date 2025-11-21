b5a4400908156c05a7f0b76eb719ab61ee1cff61
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2025-11-21 16:44:51 +0100

    refactor into high level vertical.interpolation module

--- src/earthkit/meteo/vertical/__init__.py
+++ src/earthkit/meteo/vertical/__init__.py
@@ -17,3 +17,4 @@ planned to work with objects like *earthkit.data FieldLists* or *xarray DataSets
 
 
 from .vertical import *  # noqa
+from .interpolaton import *  # noqa
--- src/earthkit/meteo/vertical/vertical.py
+++ src/earthkit/meteo/vertical/vertical.py
@@ -44,15 +44,3 @@ def geometric_height_from_geopotential_height(*args, **kwargs):
 
 def geometric_height_from_geopotential(*args, **kwargs):
     return array.geometric_height_from_geopotential(*args, **kwargs)
-
-
-def interpolate_k2p(*args, **kwargs):
-    return array.interpolate_k2p(*args, **kwargs)
-
-
-def interpolate_k2theta(*args, **kwargs):
-    return array.interpolate_k2theta(*args, **kwargs)
-
-
-def interpolate_k2any(*args, **kwargs):
-    return array.interpolate_k2any(*args, **kwargs)
